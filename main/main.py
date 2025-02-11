5a47d9c89177425dcf912cefe33d7e9c63313d0e
fathanzys <fathanamanda12@gmail.com>
2025-02-11 10:30:02 +0700

    new

--- main/main.py
+++ main/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib as pyplot
 
-data = pd.read_csv (r'C:\Users\fathan\OneDrive\Desktop\PERKULIAHAN\Semester 5\AI & ML\tugas besar\Preprocessing-Data\data\Student Depression Dataset.csv')
+data = pd.read_csv (r'C:\Users\fathan\OneDrive\Desktop\PERKULIAHAN\Semester 5\AI & ML\tugas besar\Preprocessing-Data\test.csv')
 print(data.shape)
 print(data)
 print(data.isnull().sum())
